9cf00eaf35d9a54bfae591b1f2f72fe10bb6ffe1
skynet <deluciaug@gmail.com>
2021-03-19 17:31:13 +0000

    added url https api

--- src/utility/settings.js
+++ src/utility/settings.js
@@ -1,7 +1,7 @@
 const PRODUCTION_KEY='6LfyV3MaAAAAAP1uedu-eJO3GX3PYjnu3Ko5g1-i';
 const LOCAL_KEY = '6LeSpmcaAAAAAD6W4sQkMSj6Jpsm2ghHCJlsV3UW';
 const SERVER_PROXY_LOCAL = "http://localhost:8080";
-const SERVER_PROXY_PRODUCTION = "http://68.183.3.228:8080";
+const SERVER_PROXY_PRODUCTION = "https://68.183.3.228:8080";
 const DOMAIN_NAME_LOCAL = "";
 const DOMAIN_NAME_PRODUCTION = ".reporekt.com";
 
