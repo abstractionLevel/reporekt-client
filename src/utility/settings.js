7cf5b6c0701bbb2cc297102c43128c6acf4bae0b
skynet <deluciaug@gmail.com>
2021-03-21 18:51:47 +0000

    meta tag index.htmll

--- src/utility/settings.js
+++ src/utility/settings.js
@@ -1,7 +1,8 @@
 const PRODUCTION_KEY='6LfyV3MaAAAAAP1uedu-eJO3GX3PYjnu3Ko5g1-i';
 const LOCAL_KEY = '6LeSpmcaAAAAAD6W4sQkMSj6Jpsm2ghHCJlsV3UW';
 const SERVER_PROXY_LOCAL = "http://localhost:8080";
-const SERVER_PROXY_PRODUCTION = "https://api.reporekt.com";
+const SERVER_PROXY_PRODUCTION = "http://api.reporekt.com/";
+
 //gg
 const DOMAIN_NAME_LOCAL = "";
 const DOMAIN_NAME_PRODUCTION = ".reporekt.com";
