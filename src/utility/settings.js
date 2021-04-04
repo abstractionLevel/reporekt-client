d59e01c71f095657647356daa11907aa189bc03e
skynet <deluciaug@gmail.com>
2021-04-04 21:35:02 +0100

    d

--- src/utility/settings.js
+++ src/utility/settings.js
@@ -1,16 +1,16 @@
-const PRODUCTION_KEY='6LfyV3MaAAAAAP1uedu-eJO3GX3PYjnu3Ko5g1-i';
+const PRODUCTION_KEY = '6LfyV3MaAAAAAP1uedu-eJO3GX3PYjnu3Ko5g1-i';
 const LOCAL_KEY = '6LeSpmcaAAAAAD6W4sQkMSj6Jpsm2ghHCJlsV3UW';
 const SERVER_PROXY_LOCAL = "http://localhost:8080";
-const SERVER_PROXY_PRODUCTION = "https://api.reporekt.com/";
+const SERVER_PROXY_PRODUCTION = "https://api.reporekt.com";
 
 //gg
 const DOMAIN_NAME_LOCAL = "";
 const DOMAIN_NAME_PRODUCTION = ".reporekt.com";
 
 const Settings = {
-    KEY_CAPTCHA:PRODUCTION_KEY,
-    SERVER_PROXY:SERVER_PROXY_PRODUCTION,
-    DOMAIN_NAME: DOMAIN_NAME_PRODUCTION                                                                  
-                
-  }
+  KEY_CAPTCHA: DOMAIN_NAME_PRODUCTION,
+  SERVER_PROXY: SERVER_PROXY_PRODUCTION,
+  DOMAIN_NAME: DOMAIN_NAME_PRODUCTION
+
+}
 export default Settings;
